1b2ba538f4e4d51d7a42d43bc3eadbaffb45850d
GamplaxPramgrammer <54183631+GamplaxPramgrammer@users.noreply.github.com>
2022-01-28 18:24:02 -0600

    Bug Fixes and Reticle
    Fixed a bug in which the glow stick launches from player rather than from the logical light location.

--- Darkened(DevName)/Scripts/Reticle.gd
+++ Darkened(DevName)/Scripts/Reticle.gd
@@ -8,6 +8,7 @@ extends Position2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Reticle/SketchyEffect.play("Sketchy")
 	pass # Replace with function body.
 
 
