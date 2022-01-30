75383eb60aa6dac371df0860878af9bec471b3d6
HeartBox12 <maximosity1@gmail.com>
2022-01-30 10:44:31 -0800

    Misc
    I dunno what these are.

--- Darkened(DevName)/Scripts/Greedeer.gd
+++ Darkened(DevName)/Scripts/Greedeer.gd
@@ -14,3 +14,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+
+func _on_found(area_rid, area, area_shape_index, local_shape_index):
+	pass # Replace with function body.
