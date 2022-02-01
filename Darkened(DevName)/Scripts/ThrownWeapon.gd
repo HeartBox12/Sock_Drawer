5e2687e026303d2104225b809ec334b488664b87
GamplaxPramgrammer <54183631+GamplaxPramgrammer@users.noreply.github.com>
2022-01-31 20:40:32 -0600

    Mothed Weapon now has animations for flickering off

--- Darkened(DevName)/Scripts/ThrownWeapon.gd
+++ Darkened(DevName)/Scripts/ThrownWeapon.gd
@@ -3,9 +3,6 @@ extends RigidBody2D
 func _ready():
 	GameEvents.connect("greedeer_active", self, "on_greedeer_active")
 	
-#func _process(delta):
-#	pass
-
 
 func _on_lifespan_over():
 	$Flickerout.play("TurnOff")
