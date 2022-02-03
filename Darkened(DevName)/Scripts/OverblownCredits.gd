d5eafde4860653422231b4fc8b02bd9a3e01e9c7
GamplaxPramgrammer <gameplanet459@gmail.com>
2022-02-02 23:07:52 -0600

    Credits Adjustment
    Need to credit the people who made the fonts, plus I added sound to each credit reveal.

--- Darkened(DevName)/Scripts/OverblownCredits.gd
+++ Darkened(DevName)/Scripts/OverblownCredits.gd
@@ -2,18 +2,35 @@ extends Control
 
 var loops = 0
 
+func _ready():
+	$RevealSFX.play()
+
 func _on_DramaPause_timeout():
 	match loops:
 		1:
+			$RevealSFX.play()
+			
 			$BoxCredit.visible = false
 			$WobbCredit.visible = true
 		2:
+			$RevealSFX.play()
+			
 			$WobbCredit.visible = false
 			$SugarCredit.visible = true
 		3:
+			$RevealSFX.play()
+			
 			$SugarCredit.visible = false
 			$ThorneCredit.visible = true
 		4:
+			$RevealSFX.play()
+			
+			$ThorneCredit.visible = false
+			$FontCredit.visible = true
+		6:
+			$RevealSFX.play()
+			$FontCredit.visible = false
+		7:
 			get_tree().change_scene("res://Scenes/Menus/LogoScreen.tscn")
 	
 	loops = loops + 1
