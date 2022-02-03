7cbb5afcfb65ffb7c3845555e8260fc9d1188840
GamplaxPramgrammer <gameplanet459@gmail.com>
2022-02-03 00:06:14 -0600

    Adjustments
    Got in some instructions
    and door sprites

---
+++ Darkened(DevName)/Scripts/EnemTut.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+func _ready():
+	GameEvents.connect("player_controlling", self, "_on_player_controlling")
+	GameEvents.connect("level_clear", self, "_on_level_clear")
+	$Instructions/Scribble.play("Scribbly")
+
+func _on_level_clear():
+	MusicController.stop_music()
+	
+func _on_player_controlling():
+	MusicController.play_music("res://Sounds/Spooky Ambient.mp3")
